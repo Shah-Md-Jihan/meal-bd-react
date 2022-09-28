5328ce1dda85eb7c4d172b58fa2cb51a43db87dc
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-09-28 19:44:57 +0600

    header component updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
     return (
         <div className='header-container'>
             <div className='logo'>
-                <h1>Meal<span>BD.</span></h1>
+                <h1>Earth<span>BD.</span></h1>
             </div>
             <div className='nav'>
                 <a href="/home">Home</a>
                 <a href="/about">About</a>
-                <a href="/fastfood">Fastfood</a>
-                <a href="/drinks">Drinks</a>
+                <a href="/fastfood">Countries</a>
+                <a href="/drinks">Languages</a>
                 <a href="/contact">Contact</a>
             </div>
         </div>
